4720cbb1e4e1b1b830c635e376d9ccc9e89c362c
cerihughes <github@cerihughes.co.uk>
2023-11-05 14:09:51 +0000

    Merge

--- Package.swift
+++ Package.swift
@@ -5,11 +5,22 @@ let package = Package(
     name: "Madog",
     platforms: [.iOS(.v16)],
     products: [
-        .library(name: "Madog", targets: ["MadogCore", "MadogContainers_iOS"])
+        .library(name: "Madog", targets: ["MadogContainers_iOS"])
+    ],
+    dependencies: [
+        .package(
+            name: "MadogCore",
+            url: "https://github.com/cerihughes/madog-core",
+            .branch("prepare-for-initial-release")
+        ),
+        .package(name: "KIF", url: "https://github.com/kif-framework/KIF", .exact("3.8.6"))
     ],
     targets: [
-        .target(name: "MadogCore", path: "MadogCore"),
         .target(name: "MadogContainers_iOS", dependencies: ["MadogCore"], path: "MadogContainers_iOS"),
-        .testTarget(name: "MadogCoreTests", dependencies: ["MadogCore"], path: "MadogCoreTests")
+        .testTarget(
+            name: "MadogContainersTests_iOS",
+            dependencies: ["MadogContainers_iOS", "KIF"],
+            path: "MadogContainersTests_iOS"
+        )
     ]
 )
