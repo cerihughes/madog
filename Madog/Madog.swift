1900d21e0b04ab5df114853a3ea5f7f7d63eb9d6
cerihughes <github@cerihughes.co.uk>
2023-11-05 14:47:05 +0000

    Add a static initialiser

--- Madog/Madog.swift
+++ Madog/Madog.swift
@@ -1,2 +1,10 @@
 @_exported import MadogCore
 @_exported import MadogContainers_iOS
+
+public extension Madog {
+    static func withDefaultContainers() -> Self {
+        let madog: Self = .init()
+        madog.registerDefaultContainers()
+        return madog
+    }
+}
