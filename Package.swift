7f7a1f9e0c2e762f3a25879e9434401c53c81b90
cerihughes <cerihughes@users.noreply.github.com>
2023-11-07 16:14:47 +0000

    Bump MadogCore (to use Provident) (#150)
    * Bump MadogCore (to use Provident)
    
    * Fix lint violation

--- Package.swift
+++ Package.swift
@@ -8,12 +8,18 @@ let package = Package(
         .library(name: "Madog", targets: ["Madog"])
     ],
     dependencies: [
-        .package(name: "MadogCore", url: "https://github.com/cerihughes/madog-core", .exact("1.0.0")),
-        .package(name: "KIF", url: "https://github.com/kif-framework/KIF", .exact("3.8.9"))
+        .package(url: "https://github.com/cerihughes/madog-core", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/kif-framework/KIF", .upToNextMajor(from: "3.8.0"))
     ],
     targets: [
         .target(name: "Madog", dependencies: ["MadogContainers_iOS"], path: "Madog"),
-        .target(name: "MadogContainers_iOS", dependencies: ["MadogCore"], path: "MadogContainers_iOS"),
+        .target(
+            name: "MadogContainers_iOS",
+            dependencies: [
+                .product(name: "MadogCore", package: "madog-core")
+            ],
+            path: "MadogContainers_iOS"
+        ),
         .testTarget(
             name: "MadogContainersTests_iOS",
             dependencies: ["MadogContainers_iOS", "KIF"],
